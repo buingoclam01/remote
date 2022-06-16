b5bcc5e7ac9b97d440d807b1d3c55b5be27087d7
buingoclam <lb130801@gmail.com>
2022-06-16 15:15:07 +0700

    update

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,82 @@
+<footer class="text-center text-lg-start text-white pt-5">
+<div class="footer">
+    <section class="">
+        <div class=" text-center text-md-start ">
+            <div class="row footer_content">
+                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                    <div class="footer-logo " href="#">
+                        <img  src="{{ asset('images/hapo_white.png') }}" alt="HapoLearn Logo">
+                    </div>
+                    <p class="slogan_company">
+                        Interactive lessons, "on-the-go" practice, peer support
+                    </p>
+                </div>
+
+                <div class="col-md-1 col-lg-1 col-xl-1 mx-auto">
+                    <p>
+                        <a href="#!" class="text-reset">Home</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">List course</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">List lesson</a>
+                    </p>
+                   
+                </div>
+                <div class="col-md-1 col-lg-1 col-xl-1 mx-auto p-0">
+                    <p>
+                        <a href="#!" class="text-reset">Lesson detail</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Login/Register</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Profile</a>
+                    </p>
+                </div>
+
+                
+
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4 ">
+                   <div class="list_icon">
+                     <p class="icon_item">
+                        <a href=""><i class="fa-brands fa-facebook"></i></a>
+                    </p>
+                    <p class="icon_item">
+                        <a href=""><i class="fa-solid fa-envelope"></i></a>
+                    </p>
+                    <p class="icon_item">
+                        <a href=""><i class="fa-solid fa-phone"></i></a>
+                    </p>   
+                   </div>
+                   <div class="list_address">
+                    <p>
+                        <a href="">
+                            facebook.com/tuyen.dung.haposoft
+                        </a> 
+                    </p>
+                    <p>
+                        <a href="">
+                            info@haposoft.com
+                        </a>
+                    </p>
+                    <p>
+                        <a href="">
+                            +84-85-645-9898
+                        </a>
+                    </p>
+                   </div>
+                   
+                       
+                   
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+
+<div class="copyright">
+     © 2020 HapoLearn, Inc. All rights reserved.
+</div>
+</footer>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,30 @@
+<header class="main-header">
+    <nav class="navbar navbar-expand-lg navbar-light bg-white h-100 shadow-sm">
+        <div class="header_content">
+            <a class="navbar-brand" href="#">
+                <img class="logo-image" src="{{ asset('images/logo.png') }}" alt="HapoLearn Logo">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+    
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto w-100">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">All courses <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Login/Register <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">profile <span class="sr-only">(current)</span></a>
+                    </li>
+                
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
